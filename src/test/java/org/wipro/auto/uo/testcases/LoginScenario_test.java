890c993c6200ec0608bdd9afc79b10adc8f56f7e
Santosh Modi <modisantosh@gmail.com>
2025-04-27 16:57:06 +0530

    added 2nd test case

--- src/test/java/org/wipro/auto/uo/testcases/LoginScenario_test.java
+++ src/test/java/org/wipro/auto/uo/testcases/LoginScenario_test.java
@@ -21,6 +21,19 @@ public class LoginScenario_test extends InitiateBrowser
 		
 	}
 	
+	@Test
+	public void tc02_validate_valid_login_func() throws Exception
+	{
+		LoginPage login = new LoginPage(driver);
+		
+		login.enter_username("standard_user");
+		login.enter_password("secret_sauce");
+		login.click_loginbttn();
+		
+		//because "this.driver" is null
+		
+	}
+	
 	//this test case read data from properties file
 	@Test
 	public void tc01_validate_invalid_login_func() throws Exception
